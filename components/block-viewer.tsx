c44ccd17e418cb92f6b699b8728dcbef08fd9e0d
Ronny Badilla <info@ronnybadilla.com>
2025-04-24 16:00:15 -0600

    feat: preview tab functionality

--- components/block-viewer.tsx
+++ components/block-viewer.tsx
@@ -3,11 +3,13 @@
 import { createContext, useContext, useState } from "react";
 import { type Block } from "@/types/block";
 import { Tabs, TabsTrigger, TabsList, TabsContent } from "@/components/ui/tabs";
+import { ResizablePanel, ResizablePanelGroup, ResizableHandle } from "@/components/ui/resizable";
+import Image from "next/image";
 
 type BlockViewerContext = {
   view: "preview" | "code" | "docs";
   setView: (view: "preview" | "code" | "docs") => void;
-  block: Block | null;
+  block: Block;
 }
 
 const BlockViewerContext = createContext<BlockViewerContext | null>(null);
@@ -20,14 +22,14 @@ function useBlockViewer() {
   return context;
 }
 
-function BlockViewerProvider({ children }: { children: React.ReactNode }) {
+function BlockViewerProvider({ block, children }: { block: Block, children: React.ReactNode }) {
   const [view, setView] = useState<"preview" | "code" | "docs">("preview");
-  const [block] = useState<Block | null>(null);
+  const [currentBlock] = useState<Block>(block);
 
   return (
-    <BlockViewerContext.Provider value={{ view, setView, block }}>
+    <BlockViewerContext.Provider value={{ view, setView, block: currentBlock }}>
       <div
-        id={block?.id}
+        id={currentBlock.id}
         data-view={view}
         className="group/block-view-wrapper flex min-w-0 flex-col items-stretch gap-4"
         style={
@@ -73,7 +75,8 @@ function BlockToolbar() {
   )
 }
 
-function BlockCode({ block }: { block: Block }) {
+function BlockCode() {
+  const { block } = useBlockViewer();
   return (
     <TabsContent value="code">
       <div>
@@ -84,7 +87,8 @@ function BlockCode({ block }: { block: Block }) {
   )
 }
 
-function BlockDocs({ block }: { block: Block }) {
+function BlockDocs() {
+  const { block } = useBlockViewer();
   return (
     <TabsContent value="docs">
       <div>
@@ -95,12 +99,43 @@ function BlockDocs({ block }: { block: Block }) {
   )
 }
 
-function BlockPreview({ block }: { block: Block }) {
+function BlockPreview() {
+  const { block } = useBlockViewer();
   return (
     <TabsContent value="preview">
-      <div>
-        <h2>Preview</h2>
-        <pre>{JSON.stringify(block, null, 2)}</pre>
+      <div className="grid w-full gap-4">
+        <ResizablePanelGroup direction="horizontal" className="relative z-10">
+          <ResizablePanel 
+            className="relative aspect-[4/2.5] rounded-xl border bg-background md:aspect-auto"
+            defaultSize={100}
+            minSize={30}
+          >
+            {/* TODO: add images for mobile and dark/light mode */}
+            <Image
+              src="https://ui.shadcn.com/r/styles/new-york/login-01-light.png"
+              alt="Block Preview"
+              data-block={block.id}
+              width={1440}
+              height={900}
+              className="object-cover dark:hidden md:hidden md:dark:hidden"
+            />
+            <Image
+              src="https://ui.shadcn.com/r/styles/new-york/login-01-dark.png"
+              alt="Block Preview"
+              data-block={block.id}
+              width={1440}
+              height={900}
+              className="hidden object-cover dark:block md:hidden md:dark:hidden"
+            />
+            <iframe
+              src={`https://ui.shadcn.com/view/styles/new-york/login-01`}
+              height={930}
+              className="relative z-20 hidden w-full bg-background md:block"
+            />
+          </ResizablePanel>
+          <ResizableHandle className="relative hidden w-3 bg-transparent p-0 after:absolute after:right-0 after:top-1/2 after:h-8 after:w-[6px] after:-translate-y-1/2 after:translate-x-[-1px] after:rounded-full after:bg-border after:transition-all after:hover:h-10 md:block" />
+          <ResizablePanel defaultSize={0} minSize={0} />
+        </ResizablePanelGroup>
       </div>
     </TabsContent>
   )
@@ -108,11 +143,11 @@ function BlockPreview({ block }: { block: Block }) {
 
 function BlockViewer({ block }: { block: Block }) {
   return (
-    <BlockViewerProvider>
+    <BlockViewerProvider block={block}>
       <BlockToolbar />
-      <BlockPreview block={block} />
-      <BlockCode block={block} />
-      <BlockDocs block={block} />
+      <BlockPreview />
+      <BlockCode />
+      <BlockDocs />
     </BlockViewerProvider>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ui.shadcn.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
