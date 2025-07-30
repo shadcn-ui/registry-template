af8c0a5f7e583e617e23cb7c66532e359efd24dc
Jarrod Watts <jarrod@cubelabs.xyz>
2025-07-30 16:18:52 +1000

    wtf

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export const metadata: Metadata = {
     default: siteConfig.name,
     template: `%s - ${siteConfig.name}`,
   },
-  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
   description: siteConfig.description,
   keywords: ["Next.js", "React", "Tailwind CSS", "Components", "shadcn"],
   authors: [
@@ -30,13 +29,12 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: process.env.NEXT_PUBLIC_APP_URL!,
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
     images: [
       {
-        url: `${process.env.NEXT_PUBLIC_APP_URL}/opengraph-image.png`,
+        url: `/opengraph-image.png`,
         width: 1200,
         height: 630,
         alt: siteConfig.name,
@@ -47,7 +45,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [`${process.env.NEXT_PUBLIC_APP_URL}/opengraph-image.png`],
+    images: [`/opengraph-image.png`],
     creator: "@jarrodwattsdev",
   },
   icons: {
