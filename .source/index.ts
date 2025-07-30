db5e7f7b9ad1ccdbb1c429d3dacd4af31e6386e2
Jarrod Watts <jarrod@cubelabs.xyz>
2025-07-30 13:23:27 +1000

    ok we have something

---
+++ .source/index.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck -- skip type checking
+import * as docs_2 from "../content/docs/components/accordion.mdx?collection=docs&hash=1753845077214"
+import * as docs_1 from "../content/docs/(root)/index.mdx?collection=docs&hash=1753845077214"
+import * as docs_0 from "../content/docs/(root)/about.mdx?collection=docs&hash=1753845077214"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"(root)/about.mdx","absolutePath":"/Users/jarrod/agw-reusables/content/docs/(root)/about.mdx"}, data: docs_0 }, { info: {"path":"(root)/index.mdx","absolutePath":"/Users/jarrod/agw-reusables/content/docs/(root)/index.mdx"}, data: docs_1 }, { info: {"path":"components/accordion.mdx","absolutePath":"/Users/jarrod/agw-reusables/content/docs/components/accordion.mdx"}, data: docs_2 }], [])
